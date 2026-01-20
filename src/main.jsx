a19d808e8f25cc23b54fcefadd75d2b23d828331
Abdur Rehman <abdurrehmananqayyum@gmail.com>
2026-01-20 23:48:09 +0500

    Initial Commit

--- src/main.jsx
+++ src/main.jsx
@@ -13,4 +13,4 @@
 
 import { createRoot } from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
-root.render(<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, perspiciatis.</p>)
+root.render(<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, perspiciatis.</p>, <h2>Happy Working Days</h2>)
