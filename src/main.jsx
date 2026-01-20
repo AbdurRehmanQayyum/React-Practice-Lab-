3e8f2beb6fdcaf8d9a0fce47b82404fdf817bc0f
Abdur Rehman <abdurrehmananqayyum@gmail.com>
2026-01-20 23:40:33 +0500

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+import { createRoot } from "root-dom/client"
+const root =createRoot(document.getElementById("root"))
+root.render(<h1>Hello Abdur Rehman!</h1>)
