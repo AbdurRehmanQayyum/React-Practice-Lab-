5fb015b2567f6b54591dc14bbaa708e875a374fa
Abdur Rehman <abdurrehmananqayyum@gmail.com>
2026-01-21 18:31:05 +0500

    initial commits

--- src/main.jsx
+++ src/main.jsx
@@ -11,11 +11,12 @@
 
 
 import { createRoot } from "react-dom/client"
-const root = createRoot(document.getElementById("root"))
-root.render(<p>Why are you doing react ?</p>,
+const root = createRoot(document.querySelector("#root"))
+root.render(<div><p>Why are you doing react ?</p>
 <ul>
   <li>Money</li>
   <li>Skill</li>
   <li>Freedom of</li>
-</ul>)
+</ul>
+</div>)
 
