123978f6a2fe9468f188cb324181e75a964a071b
Abdur Rehman <abdurrehmananqayyum@gmail.com>
2026-01-29 13:29:13 +0500

    Adding Components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
-import logo from "./assets/images/react-img.png";
+import logo from "../assets/images/react-img.png";
 
-<header>
+
+function Header() {
+    return (
+    <header>
       <img src={logo} width="90px" alt="Logo" />
     </header>
+    );
+}
+
 
 export default Header;
